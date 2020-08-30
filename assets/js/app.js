880c747039f93a3ac59413d3f28222966a2fa5a4
onenaghi <onenaghi@yahoo.com>
2020-08-29 22:33:57 -0500

    set up chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-// @TODO: YOUR CODE HERE!
+// Step 1: Set up our chart
+//= ================================
+
+// Step 1: Set up our chart
+//= ================================
+const svgWidth = 960;
+const svgHeight = 500;
+
+const margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
+
