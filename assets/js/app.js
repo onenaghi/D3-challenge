9cf0d4a05d0edc2f685d6ecb012d1eda0787d560
onenaghi <onenaghi@yahoo.com>
2020-08-29 23:04:36 -0500

    create scatter plot circles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,5 @@
 // Step 1: Set up our chart
 //= ================================
-
-// Step 1: Set up our chart
-//= ================================
 const svgWidth = 960;
 const svgHeight = 500;
 
@@ -20,16 +17,88 @@ const height = svgHeight - margin.top - margin.bottom;
 // append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 // =================================
-const svg = d3
-  .select("body")
+var svg = d3.select("#scatter")
   .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
 
 const chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Step 3:
-// Import data from the donuts.csv file
+// Import data from the data.csv file
+// and define values to be used
 // =================================
-d3.csv("../data/data.csv").then(function(censusData) {
+d3.csv('assets/data/data.csv').then(function (censusData) {
+    console.log(censusData);
+    censusData.forEach(function(data){
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare
+    })
+
+// Create a scale for your independent (x) coordinates
+const xScale = d3.scaleLinear()
+.domain([d3.min(censusData, d=>d.poverty)*.95,d3.max(censusData, d=>d.poverty)])
+.range([0,width]);
+
+    // Create a scale for your dependent (y) coordinates
+const yScale = d3.scaleLinear()
+.domain([d3.min(censusData, d=>d.healthcare)*.85, d3.max(censusData, d=>d.healthcare)])
+.range([height, 0]);
+
+// Use bottomAxis and leftAxis to create the chart's axes using the passed in scales.
+var bottomAxis = d3.axisBottom(xScale);
+var leftAxis = d3.axisLeft(yScale);
+
+    // append x axis
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .style("font-size", "18px")
+        .call(bottomAxis);
+
+    // append y axis
+    chartGroup.append("g")
+        .style("font-size", "18px")
+        .call(leftAxis);
+
+  // Create Circles
+  // ==============================
+  const circlesGroup = chartGroup.selectAll("circle")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(d.poverty))
+    .attr("cy", d => yScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "violet")
+    .attr("opacity", ".3");
+
+    const textGroup = chartGroup.selectAll("text.abbr")
+    .data(censusData)
+    .enter()
+    .append("text")
+    .attr("class","abbr")
+    .attr("x", d => xScale(d.poverty)-5)
+    .attr("y", d => yScale(d.healthcare)+2)
+    .attr("opacity","0.5")
+    .text(d=>d.abbr)
+
+// Create axes labels
+chartGroup.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 0 - chartMargin.left + 40)
+.attr("x", 0 - (height / 2)-100)
+.attr("dy", "1em")
+.attr("class", "axisText")
+.text("Lacks Healthcare (%)");
+
+chartGroup.append("text")
+.attr("transform", `translate(${width / 2}, ${height + chartMargin.top+30})`)
+.attr("class", "axisText")
+.text("In Poverty (%)");
+}).catch(function (error) {
+console.log(error);
+
+
+});
