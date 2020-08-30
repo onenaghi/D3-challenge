c20bc3c861c9eba285a0409c879898fc1b70bf98
onenaghi <onenaghi@yahoo.com>
2020-08-29 22:42:25 -0500

    psuedocode and svg

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,20 @@ const margin = {
 const width = svgWidth - margin.left - margin.right;
 const height = svgHeight - margin.top - margin.bottom;
 
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+const chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Step 3:
+// Import data from the donuts.csv file
+// =================================
+d3.csv("../data/data.csv").then(function(censusData) {
